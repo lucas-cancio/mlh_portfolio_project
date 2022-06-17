5418886c87886d9701dd6f93c92def39aedd1a95
Lucas Cancio <lucas.canciox1@gmail.com>
2022-06-17 20:40:52 +0000

    added redeploy bash script

---
+++ redeploy-site.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pkill -f tmux
+
+cd mlh_portfolio_project
+git fetch && git reset origin/main --hard
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+
+tmux new -d 'source python3-virtualenv/bin/activate && export FLASK_APP=app/server && flask run --host=0.0.0.0'
