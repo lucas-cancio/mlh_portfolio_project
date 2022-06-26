11d8604cc022d108df249d5ebf44505589cf05d3
Lucas Cancio <lucas.canciox1@gmail.com>
2022-06-26 19:13:05 +0000

    added requests to requirements.txt and reverted redeploy script to original

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -5,7 +5,6 @@ pkill -f tmux
 cd mlh_portfolio_project
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
-pip3 freeze > requirements.txt
 pip install -r requirements.txt
 
 tmux new -d 'source python3-virtualenv/bin/activate && export FLASK_APP=app/server && flask run --host=0.0.0.0'
