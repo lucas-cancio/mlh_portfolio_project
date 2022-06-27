0e6a8d2bec75e66c2c23f2b15bd4a0741102d148
Lucas Cancio <lucas.canciox1@gmail.com>
2022-06-27 22:33:36 +0000

    Renamed app/server.py back to app/__init__.py. Also changed the redeploy script to use services instead of a tmux server

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -7,4 +7,5 @@ git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new -d 'source python3-virtualenv/bin/activate && export FLASK_APP=app/server && flask run --host=0.0.0.0'
+#tmux new -d 'source python3-virtualenv/bin/activate && export FLASK_APP=app/server && flask run --host=0.0.0.0'
+export FLASK_APP=app/__init__.py
