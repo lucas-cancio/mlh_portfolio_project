8eeaff71faa78bd3c0e3f3884824d4f51cacb657
Lucas Cancio <lucas.canciox1@gmail.com>
2022-06-27 22:39:25 +0000

    Also changed the redeploy script to use services instead of a tmux server

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -9,3 +9,6 @@ pip install -r requirements.txt
 
 #tmux new -d 'source python3-virtualenv/bin/activate && export FLASK_APP=app/server && flask run --host=0.0.0.0'
 export FLASK_APP=app/__init__.py
+systemctl daemon-reload
+systemctl restart myportfolio
+systemctl status myportfolio
