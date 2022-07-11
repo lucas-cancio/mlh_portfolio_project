bc7547e9c81e76ae196a4c6bfc74e2b2d45d0d06
Lucas Cancio <lucas.canciox1@gmail.com>
2022-07-11 22:28:33 +0000

    Got docker compose file working in production env and changed redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -7,8 +7,5 @@ git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-#tmux new -d 'source python3-virtualenv/bin/activate && export FLASK_APP=app/server && flask run --host=0.0.0.0'
-export FLASK_APP=app/__init__.py
-systemctl daemon-reload
-systemctl restart myportfolio
-systemctl status myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
